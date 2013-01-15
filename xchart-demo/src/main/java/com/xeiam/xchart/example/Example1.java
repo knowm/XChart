798813a3fbebc0e49c7a96a103a8ba4544d3c992
Tim Molter <tim.molter@gmail.com>
2013-01-15 08:33:27 +0100

    clean up

--- xchart-demo/src/main/java/com/xeiam/xchart/example/Example1.java
+++ xchart-demo/src/main/java/com/xeiam/xchart/example/Example1.java
@@ -15,11 +15,8 @@
  */
 package com.xeiam.xchart.example;
 
-import java.util.Arrays;
-import java.util.Collection;
-
+import com.xeiam.xchart.BitmapEncoder;
 import com.xeiam.xchart.Chart;
-import com.xeiam.xchart.SwingWrapper;
 
 /**
  * Creates a simple Chart and saves it as a PNG and JPEG image file.
@@ -32,29 +29,15 @@ public class Example1 {
 
     double[] yData = new double[] { 2.0, 1.0, 0.0 };
 
-    Collection<Number> yData2 = Arrays.asList(new Number[] { 0.0, 1.0, 2.0 });
-
-    Collection<Number> yData3 = Arrays.asList(new Number[] { 0.0, 0.0, 2.0 });
-
-    Collection<Number> yData4 = Arrays.asList(new Number[] { 0.0, 3.0, 2.0 });
-
-    Collection<Number> yData5 = Arrays.asList(new Number[] { 3.0, 3.0, 2.0 });
-
     // Create Chart
     Chart chart = new Chart(500, 400);
     chart.setTitle("Sample Chart");
     chart.setXAxisTitle("X");
     chart.setYAxisTitle("Y");
     chart.addSeries("y(x)", null, yData);
-    chart.addSeries("w(t)", null, yData2);
-    chart.addSeries("f(k)", null, yData3);
-    chart.addSeries("f(h)", null, yData4);
-    chart.addSeries("g(h)", null, yData5);
 
-    new SwingWrapper(chart).displayChart();
-
-    // BitmapEncoder.savePNG(chart, "./Sample_Chart.png");
-    // BitmapEncoder.saveJPG(chart, "./Sample_Chart.jpg", 0.95f);
+    BitmapEncoder.savePNG(chart, "./Sample_Chart.png");
+    BitmapEncoder.saveJPG(chart, "./Sample_Chart.jpg", 0.95f);
 
   }
 }
