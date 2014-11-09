df5cc7c70cb7ceed8ec36946a1fe1646d28a24ce
Benedikt Bünz <karlbenedikt.buenz@uzh.ch>
2014-11-09 01:06:34 -0800

    implemented Vector graphics saveas

--- xchart/src/main/java/com/xeiam/xchart/XChartPanel.java
+++ xchart/src/main/java/com/xeiam/xchart/XChartPanel.java
@@ -39,345 +39,305 @@ import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 
 import com.xeiam.xchart.BitmapEncoder.BitmapFormat;
+import com.xeiam.xchart.VectorGraphicsEncoder.VectorGraphicsFormat;
 
 /**
  * A Swing JPanel that contains a Chart
  * <p>
- * Right-click + Save As... or ctrl+S pops up a Save As dialog box for saving the chart as a JPeg or PNG file.
+ * Right-click + Save As... or ctrl+S pops up a Save As dialog box for saving
+ * the chart as a JPeg or PNG file.
  *
  * @author timmolter
  */
 public class XChartPanel extends JPanel {
 
-  private final Chart chart;
-  private final Dimension preferredSize;
-  private String saveAsString = "Save As...";
+    private final Chart chart;
+    private final Dimension preferredSize;
+    private String saveAsString = "Save As...";
 
-  /**
-   * Constructor
-   *
-   * @param chart
-   */
-  public XChartPanel(final Chart chart) {
+    /**
+     * Constructor
+     *
+     * @param chart
+     */
+    public XChartPanel(final Chart chart) {
 
-    this.chart = chart;
-    preferredSize = new Dimension(chart.getWidth(), chart.getHeight());
+        this.chart = chart;
+        preferredSize = new Dimension(chart.getWidth(), chart.getHeight());
 
-    // Right-click listener for saving chart
-    this.addMouseListener(new PopUpMenuClickListener());
+        // Right-click listener for saving chart
+        this.addMouseListener(new PopUpMenuClickListener());
 
-    // Control+S key listener for saving chart
-    KeyStroke ctrlS = KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-    this.getInputMap(WHEN_IN_FOCUSED_WINDOW).put(ctrlS, "save");
-    this.getActionMap().put("save", new SaveAction());
-  }
+        // Control+S key listener for saving chart
+        KeyStroke ctrlS = KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+        this.getInputMap(WHEN_IN_FOCUSED_WINDOW).put(ctrlS, "save");
+        this.getActionMap().put("save", new SaveAction());
+    }
 
-  /**
-   * Set the "Save As..." String if you want to localize it.
-   *
-   * @param saveAsString
-   */
-  public void setSaveAsString(String saveAsString) {
+    /**
+     * Set the "Save As..." String if you want to localize it.
+     *
+     * @param saveAsString
+     */
+    public void setSaveAsString(String saveAsString) {
 
-    this.saveAsString = saveAsString;
-  }
-
-  @Override
-  protected void paintComponent(Graphics g) {
-
-    super.paintComponent(g);
-
-    chart.paint((Graphics2D) g, getWidth(), getHeight());
-  }
-
-  @Override
-  public Dimension getPreferredSize() {
-
-    return this.preferredSize;
-  }
-
-  private class SaveAction extends AbstractAction {
-
-    public SaveAction() {
-
-      super("save");
+        this.saveAsString = saveAsString;
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    protected void paintComponent(Graphics g) {
 
-      showSaveAsDialog();
+        super.paintComponent(g);
+
+        chart.paint((Graphics2D) g, getWidth(), getHeight());
     }
-  }
 
-  private void showSaveAsDialog() {
+    @Override
+    public Dimension getPreferredSize() {
 
-    JFileChooser fileChooser = new JFileChooser();
-    fileChooser.addChoosableFileFilter(new JPGSaveFilter());
-    FileFilter pngFileFilter = new PNGSaveFilter();
-    fileChooser.addChoosableFileFilter(pngFileFilter);
-    fileChooser.addChoosableFileFilter(new BMPSaveFilter());
-    fileChooser.addChoosableFileFilter(new GIFSaveFilter());
-    fileChooser.setAcceptAllFileFilterUsed(false);
+        return this.preferredSize;
+    }
 
-    fileChooser.setFileFilter(pngFileFilter);
+    private class SaveAction extends AbstractAction {
 
-    if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+        public SaveAction() {
 
-      if (fileChooser.getSelectedFile() != null) {
-        File theFileToSave = fileChooser.getSelectedFile();
-        try {
-          if (fileChooser.getFileFilter() == null) {
-            BitmapEncoder.saveBitmap(chart, theFileToSave.getCanonicalPath().toString(), BitmapFormat.PNG);
-          }
-          else if (fileChooser.getFileFilter().getDescription().equals("*.jpg,*.JPG")) {
-            BitmapEncoder.saveJPGWithQuality(chart, theFileToSave.getCanonicalPath().toString() + ".jpg", 1.0f);
-          }
-          else if (fileChooser.getFileFilter().getDescription().equals("*.png,*.PNG")) {
-            BitmapEncoder.saveBitmap(chart, theFileToSave.getCanonicalPath().toString(), BitmapFormat.PNG);
-          }
-          else if (fileChooser.getFileFilter().getDescription().equals("*.bmp,*.BMP")) {
-            BitmapEncoder.saveBitmap(chart, theFileToSave.getCanonicalPath().toString(), BitmapFormat.BMP);
-          }
-          else if (fileChooser.getFileFilter().getDescription().equals("*.gif,*.GIF")) {
-            BitmapEncoder.saveBitmap(chart, theFileToSave.getCanonicalPath().toString(), BitmapFormat.GIF);
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
+            super("save");
         }
-      }
-
-    }
-  }
-
-  private class JPGSaveFilter extends FileFilter {
-
-    @Override
-    public boolean accept(File f) {
-
-      if (f.isDirectory()) {
-        return false;
-      }
-
-      String s = f.getName();
-
-      return s.endsWith(".jpg") || s.endsWith(".JPG");
-    }
-
-    @Override
-    public String getDescription() {
-
-      return "*.jpg,*.JPG";
-    }
-
-  }
-
-  private class BMPSaveFilter extends FileFilter {
-
-    @Override
-    public boolean accept(File f) {
-
-      if (f.isDirectory()) {
-        return false;
-      }
-
-      String s = f.getName();
-
-      return s.endsWith(".bmp") || s.endsWith(".BMP");
-    }
-
-    @Override
-    public String getDescription() {
-
-      return "*.bmp,*.BMP";
-    }
-
-  }
-
-  private class GIFSaveFilter extends FileFilter {
-
-    @Override
-    public boolean accept(File f) {
-
-      if (f.isDirectory()) {
-        return false;
-      }
-
-      String s = f.getName();
-
-      return s.endsWith(".gif") || s.endsWith(".GIF");
-    }
-
-    @Override
-    public String getDescription() {
-
-      return "*.gif,*.GIF";
-    }
-
-  }
-
-  private class PNGSaveFilter extends FileFilter {
-
-    @Override
-    public boolean accept(File f) {
-
-      if (f.isDirectory()) {
-        return false;
-      }
-
-      String s = f.getName();
-
-      return s.endsWith(".png") || s.endsWith(".PNG");
-    }
-
-    @Override
-    public String getDescription() {
-
-      return "*.png,*.PNG";
-    }
-
-  }
-
-  private class PopUpMenuClickListener extends MouseAdapter {
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-      if (e.isPopupTrigger()) {
-        doPop(e);
-      }
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-      if (e.isPopupTrigger()) {
-        doPop(e);
-      }
-    }
-
-    private void doPop(MouseEvent e) {
-
-      XChartPanelPopupMenu menu = new XChartPanelPopupMenu();
-      menu.show(e.getComponent(), e.getX(), e.getY());
-    }
-  }
-
-  private class XChartPanelPopupMenu extends JPopupMenu {
-
-    JMenuItem saveAsMenuItem;
-
-    public XChartPanelPopupMenu() {
-
-      saveAsMenuItem = new JMenuItem(saveAsString);
-      saveAsMenuItem.addMouseListener(new MouseListener() {
 
         @Override
-        public void mouseReleased(MouseEvent e) {
+        public void actionPerformed(ActionEvent e) {
 
-          showSaveAsDialog();
+            showSaveAsDialog();
         }
+    }
+
+    private void showSaveAsDialog() {
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.addChoosableFileFilter(new SuffixSaveFilter("jpeg"));
+        FileFilter pngFileFilter = new SuffixSaveFilter("png");
+        fileChooser.addChoosableFileFilter(pngFileFilter);
+        fileChooser.addChoosableFileFilter(new SuffixSaveFilter("bmp"));
+        fileChooser.addChoosableFileFilter(new SuffixSaveFilter("gif"));
+        fileChooser.addChoosableFileFilter(new SuffixSaveFilter("pdf"));
+        fileChooser.addChoosableFileFilter(new SuffixSaveFilter("eps"));
+        fileChooser.addChoosableFileFilter(new SuffixSaveFilter("svg"));
+        fileChooser.setAcceptAllFileFilterUsed(false);
+
+        fileChooser.setFileFilter(pngFileFilter);
+
+        if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+
+            if (fileChooser.getSelectedFile() != null) {
+                File theFileToSave = fileChooser.getSelectedFile();
+                try {
+                    String filePath = theFileToSave.getCanonicalPath().toString();
+                    if (fileChooser.getFileFilter() == null) {
+                        BitmapEncoder.saveBitmap(chart, filePath, BitmapFormat.PNG);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.jpg,*.JPG")) {
+                        BitmapEncoder.saveJPGWithQuality(chart, filePath + ".jpg", 1.0f);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.png,*.PNG")) {
+                        BitmapEncoder.saveBitmap(chart, filePath, BitmapFormat.PNG);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.bmp,*.BMP")) {
+                        BitmapEncoder.saveBitmap(chart, filePath, BitmapFormat.BMP);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.gif,*.GIF")) {
+                        BitmapEncoder.saveBitmap(chart, filePath, BitmapFormat.GIF);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.pdf,*.PDF")) {
+                        VectorGraphicsEncoder.saveVectorGraphic(chart, filePath, VectorGraphicsFormat.PDF);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.eps,*.EPS")) {
+                        VectorGraphicsEncoder.saveVectorGraphic(chart, filePath, VectorGraphicsFormat.EPS);
+                    } else if (fileChooser.getFileFilter().getDescription().equals("*.svg,*.SVG")) {
+                        VectorGraphicsEncoder.saveVectorGraphic(chart, filePath, VectorGraphicsFormat.SVG);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    /**
+     * File filter based on the suffix of a file. This file filter accepts all
+     * files that end with .suffix or the capitalized suffix.
+     * 
+     * @author Benedikt Bünz
+     *
+     */
+    private class SuffixSaveFilter extends FileFilter {
+        private final String suffix;
+
+        /**
+         * 
+         * @param suffix
+         *            This file filter accepts all files that end with .suffix
+         *            or the capitalized suffix.
+         */
+        public SuffixSaveFilter(String suffix) {
+            this.suffix = suffix;
+        }
+
+        @Override
+        public boolean accept(File f) {
+
+            if (f.isDirectory()) {
+                return false;
+            }
+
+            String s = f.getName();
+
+            return s.endsWith("." + suffix) || s.endsWith("." + suffix.toUpperCase());
+        }
+
+        @Override
+        public String getDescription() {
+
+            return "*." + suffix + ",*." + suffix.toUpperCase();
+        }
+    }
+
+    private class PopUpMenuClickListener extends MouseAdapter {
 
         @Override
         public void mousePressed(MouseEvent e) {
 
+            if (e.isPopupTrigger()) {
+                doPop(e);
+            }
         }
 
         @Override
-        public void mouseExited(MouseEvent e) {
+        public void mouseReleased(MouseEvent e) {
 
+            if (e.isPopupTrigger()) {
+                doPop(e);
+            }
         }
 
-        @Override
-        public void mouseEntered(MouseEvent e) {
+        private void doPop(MouseEvent e) {
 
+            XChartPanelPopupMenu menu = new XChartPanelPopupMenu();
+            menu.show(e.getComponent(), e.getX(), e.getY());
         }
+    }
 
-        @Override
-        public void mouseClicked(MouseEvent e) {
+    private class XChartPanelPopupMenu extends JPopupMenu {
 
+        JMenuItem saveAsMenuItem;
+
+        public XChartPanelPopupMenu() {
+
+            saveAsMenuItem = new JMenuItem(saveAsString);
+            saveAsMenuItem.addMouseListener(new MouseListener() {
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                    showSaveAsDialog();
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+            });
+            add(saveAsMenuItem);
         }
-      });
-      add(saveAsMenuItem);
     }
-  }
 
-  /**
-   * update a series by only updating the Y-Axis data. The X-Axis data will be automatically generated as a list of increasing Integers starting from 1 and ending at the size of the new Y-Axis data
-   * list.
-   *
-   * @param seriesName
-   * @param newYData
-   * @return
-   */
-  public Series updateSeries(String seriesName, Collection<? extends Number> newYData) {
+    /**
+     * update a series by only updating the Y-Axis data. The X-Axis data will be
+     * automatically generated as a list of increasing Integers starting from 1
+     * and ending at the size of the new Y-Axis data list.
+     *
+     * @param seriesName
+     * @param newYData
+     * @return
+     */
+    public Series updateSeries(String seriesName, Collection<? extends Number> newYData) {
 
-    Series series = chart.getSeriesMap().get(seriesName);
-    if (series == null) {
-      throw new IllegalArgumentException("Series name >" + seriesName + "< not found!!!");
+        Series series = chart.getSeriesMap().get(seriesName);
+        if (series == null) {
+            throw new IllegalArgumentException("Series name >" + seriesName + "< not found!!!");
+        }
+        series.replaceYData(newYData);
+
+        // generate X-Data
+        List<Integer> generatedXData = new ArrayList<Integer>();
+        for (int i = 1; i < newYData.size() + 1; i++) {
+            generatedXData.add(i);
+        }
+        series.replaceXData(generatedXData);
+
+        // Re-display the chart
+        revalidate();
+        repaint();
+        return series;
+
     }
-    series.replaceYData(newYData);
 
-    // generate X-Data
-    List<Integer> generatedXData = new ArrayList<Integer>();
-    for (int i = 1; i < newYData.size() + 1; i++) {
-      generatedXData.add(i);
+    /**
+     * update a series by updating both the X-Axis and Y-Axis data
+     *
+     * @param seriesName
+     * @param newYData
+     * @return
+     */
+    public Series updateSeries(String seriesName, Collection<?> newXData, List<? extends Number> newYData) {
+
+        Series series = chart.getSeriesMap().get(seriesName);
+        if (series == null) {
+            throw new IllegalArgumentException("Series name >" + seriesName + "< not found!!!");
+        }
+        series.replaceXData(newXData);
+        series.replaceYData(newYData);
+
+        // Re-display the chart
+        revalidate();
+        repaint();
+
+        return series;
     }
-    series.replaceXData(generatedXData);
 
-    // Re-display the chart
-    revalidate();
-    repaint();
-    return series;
+    /**
+     * update a series by updating the X-Axis, Y-Axis and error bar data
+     *
+     * @param seriesName
+     * @param newXData
+     * @param newYData
+     * @param newErrorBarData
+     * @return
+     */
+    public Series updateSeries(String seriesName, Collection<?> newXData, List<? extends Number> newYData, List<? extends Number> newErrorBarData) {
 
-  }
+        Series series = chart.getSeriesMap().get(seriesName);
+        if (series == null) {
+            throw new IllegalArgumentException("Series name >" + seriesName + "< not found!!!");
+        }
+        series.replaceXData(newXData);
+        series.replaceYData(newYData);
+        series.replaceErrroBarData(newErrorBarData);
 
-  /**
-   * update a series by updating both the X-Axis and Y-Axis data
-   *
-   * @param seriesName
-   * @param newYData
-   * @return
-   */
-  public Series updateSeries(String seriesName, Collection<?> newXData, List<? extends Number> newYData) {
+        // Re-display the chart
+        revalidate();
+        repaint();
 
-    Series series = chart.getSeriesMap().get(seriesName);
-    if (series == null) {
-      throw new IllegalArgumentException("Series name >" + seriesName + "< not found!!!");
+        return series;
     }
-    series.replaceXData(newXData);
-    series.replaceYData(newYData);
-
-    // Re-display the chart
-    revalidate();
-    repaint();
-
-    return series;
-  }
-
-  /**
-   * update a series by updating the X-Axis, Y-Axis and error bar data
-   *
-   * @param seriesName
-   * @param newXData
-   * @param newYData
-   * @param newErrorBarData
-   * @return
-   */
-  public Series updateSeries(String seriesName, Collection<?> newXData, List<? extends Number> newYData, List<? extends Number> newErrorBarData) {
-
-    Series series = chart.getSeriesMap().get(seriesName);
-    if (series == null) {
-      throw new IllegalArgumentException("Series name >" + seriesName + "< not found!!!");
-    }
-    series.replaceXData(newXData);
-    series.replaceYData(newYData);
-    series.replaceErrroBarData(newErrorBarData);
-
-    // Re-display the chart
-    revalidate();
-    repaint();
-
-    return series;
-  }
 }
