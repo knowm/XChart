c2309bf97e650fc06f86028a32c156c2ae278f88
Tim Molter <tim.molter@gmail.com>
2015-02-14 10:36:05 +0100

    update headers

--- xchart/src/main/java/com/xeiam/xchart/internal/package-info.java
+++ xchart/src/main/java/com/xeiam/xchart/internal/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 - 2014 Xeiam LLC.
+ * Copyright 2011 - 2015 Xeiam LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * Classes in this package are internal and are not intended to be accessed directly. Therefore, they are not included in the JavaDocs.
  */
