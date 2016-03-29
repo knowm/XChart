26d1c4350027d9638bbb89b5dbcb95814ef3c4a0
Tim Molter <tim@knowm.org>
2016-03-29 15:12:09 +0200

    revert example0 code

--- xchart-demo/src/main/java/org/knowm/xchart/standalone/Example0.java
+++ xchart-demo/src/main/java/org/knowm/xchart/standalone/Example0.java
@@ -16,9 +16,9 @@
  */
 package org.knowm.xchart.standalone;
 
-import org.knowm.xchart.XYChart;
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
 /**
  * Creates a simple Chart using QuickChart
@@ -27,8 +27,8 @@ public class Example0 {
 
   public static void main(String[] args) throws Exception {
 
-    double[] xData = new double[] { 1.0, 2.0 };
-    double[] yData = new double[] { 1E-12, 1.1E-12 };
+    double[] xData = new double[] { 0.0, 1.0, 2.0 };
+    double[] yData = new double[] { 2.0, 1.0, 0.0 };
 
     // Create Chart
     XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
