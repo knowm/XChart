34ccaf69f58b2cf4b8ca05cf52096e8d942f647d
Hakan Işıktekin <hakan.isiktekin@gmail.com>
2017-05-12 14:57:14 +0300

    Removed unnecessary import

--- xchart/src/main/java/org/knowm/xchart/internal/chartpart/PlotContent_Category_Line_Area_Scatter.java
+++ xchart/src/main/java/org/knowm/xchart/internal/chartpart/PlotContent_Category_Line_Area_Scatter.java
@@ -20,7 +20,6 @@ import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
-import java.awt.geom.Rectangle2D;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
