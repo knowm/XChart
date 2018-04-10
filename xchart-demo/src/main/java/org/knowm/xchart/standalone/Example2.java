b68440f95ed12fbfbefaa06c349f0208b2f4b77f
Tim Molter <tim.molter@gmail.com>
2018-04-10 09:35:52 +0200

    bitmapencoder demo for a chart matrix

--- xchart-demo/src/main/java/org/knowm/xchart/standalone/Example2.java
+++ xchart-demo/src/main/java/org/knowm/xchart/standalone/Example2.java
@@ -1,11 +1,11 @@
 package org.knowm.xchart.standalone;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.XYChart;
-import org.knowm.xchart.XYChartBuilder;
-import org.knowm.xchart.XYSeries;
+
+import org.knowm.xchart.*;
+import org.knowm.xchart.internal.chartpart.Chart;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
 /**
@@ -15,11 +15,11 @@ import org.knowm.xchart.style.markers.SeriesMarkers;
  */
 public class Example2 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
     int numCharts = 4;
 
-    List<XYChart> charts = new ArrayList<XYChart>();
+    List<Chart> charts = new ArrayList<Chart>();
 
     for (int i = 0; i < numCharts; i++) {
       XYChart chart =
@@ -30,7 +30,9 @@ public class Example2 {
       series.setMarker(SeriesMarkers.NONE);
       charts.add(chart);
     }
-    new SwingWrapper<XYChart>(charts).displayChartMatrix();
+    new SwingWrapper<Chart>(charts).displayChartMatrix();
+
+    BitmapEncoder.saveBitmap(charts,2,2, "./Sample_Chart_Matrix", BitmapEncoder.BitmapFormat.PNG);
   }
 
   /**
