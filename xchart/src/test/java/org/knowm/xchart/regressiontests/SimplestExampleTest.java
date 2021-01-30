968c8edb7126b20057795f7985ee77ad41e06860
Greg Oledzki <goledzki@sumologic.com>
2021-01-30 21:53:40 +0100

    Adding Unit Test for the simplest example

---
+++ xchart/src/test/java/org/knowm/xchart/regressiontests/SimplestExampleTest.java
@@ -0,0 +1,43 @@
+package org.knowm.xchart.regressiontests;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.XYChart;
+
+public class SimplestExampleTest {
+
+  static String digestType = "md5";
+
+  @Test
+  public void testSimplestExampleStaysTheSame() throws Exception {
+    // given
+    double[] xData = new double[] {0.0, 1.0, 2.0};
+    double[] yData = new double[] {2.0, 1.0, 0.0};
+
+    // when
+    XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+    DigestOutputStream output =
+        new DigestOutputStream(new ByteArrayOutputStream(), MessageDigest.getInstance(digestType));
+    BitmapEncoder.saveBitmap(chart, output, BitmapEncoder.BitmapFormat.PNG);
+    output.close();
+
+    // test
+    assertImagesEquals("/simplestExample.png", output);
+  }
+
+  public void assertImagesEquals(String expectedFileName, DigestOutputStream actual)
+      throws Exception {
+    byte[] expectedBytes =
+        Files.readAllBytes(Paths.get(getClass().getResource(expectedFileName).toURI()));
+    byte[] expectedDigest = MessageDigest.getInstance("MD5").digest(expectedBytes);
+
+    Assert.assertArrayEquals(expectedDigest, actual.getMessageDigest().digest());
+  }
+}
