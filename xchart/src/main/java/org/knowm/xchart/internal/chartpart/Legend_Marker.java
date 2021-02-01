10306b5711d1c6de5818ebcac93cb022c181bd10
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2021-02-01 22:14:55 +0100

    Math.min, Math.max usage

--- xchart/src/main/java/org/knowm/xchart/internal/chartpart/Legend_Marker.java
+++ xchart/src/main/java/org/knowm/xchart/internal/chartpart/Legend_Marker.java
@@ -105,9 +105,7 @@ public class Legend_Marker<ST extends AxesChartStyler, S extends MarkerSeries>
           BasicStroke existingLineStyle = series.getLineStyle();
           BasicStroke newLineStyle =
               new BasicStroke(
-                  existingLineStyle.getLineWidth() > BOX_OUTLINE_WIDTH * 0.5f
-                      ? BOX_OUTLINE_WIDTH * 0.5f
-                      : existingLineStyle.getLineWidth(),
+                      Math.min(existingLineStyle.getLineWidth(), BOX_OUTLINE_WIDTH * 0.5f),
                   existingLineStyle.getEndCap(),
                   existingLineStyle.getLineJoin(),
                   existingLineStyle.getMiterLimit(),
