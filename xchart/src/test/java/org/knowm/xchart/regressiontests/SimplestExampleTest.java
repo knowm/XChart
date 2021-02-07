170dc4557591bafadf09d848dab490de013b0f09
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2021-02-07 22:29:10 +0100

    A TTF font file to be used by unit tests for rendering

--- xchart/src/test/java/org/knowm/xchart/regressiontests/SimplestExampleTest.java
+++ xchart/src/test/java/org/knowm/xchart/regressiontests/SimplestExampleTest.java
@@ -2,19 +2,34 @@ package org.knowm.xchart.regressiontests;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
 import org.junit.Assert;
 import org.junit.Test;
-import org.knowm.xchart.BitmapEncoder;
-import org.knowm.xchart.QuickChart;
-import org.knowm.xchart.XYChart;
+import org.knowm.xchart.*;
 
 public class SimplestExampleTest {
 
   static String digestType = "md5";
+
+  Font font;
+
+  {
+    try {
+      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+      font =
+          Font.createFont(
+              Font.TRUETYPE_FONT,
+              new File(getClass().getResource("/font/Barlow-Regular.ttf").getFile()));
+      ge.registerFont(font);
+    } catch (IOException | FontFormatException e) {
+      throw new RuntimeException("Failed to load the font file for tests", e);
+    }
+  }
 
   @Test
   public void testSimplestExampleStaysTheSame() throws Exception {
@@ -24,30 +39,27 @@ public class SimplestExampleTest {
 
     // when
     XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-    chart.getStyler().setChartTitleFont(arial());
-    chart.getStyler().setAxisTickLabelsFont(arial());
-    chart.getStyler().setLegendFont(arial());
-    chart.getStyler().setAxisTitleFont(arial());
+    chart.getStyler().setChartTitleFont(fontToBeUsed());
+    chart.getStyler().setAxisTickLabelsFont(fontToBeUsed());
+    chart.getStyler().setLegendFont(fontToBeUsed());
+    chart.getStyler().setAxisTitleFont(fontToBeUsed());
     DigestOutputStream output =
         new DigestOutputStream(new ByteArrayOutputStream(), MessageDigest.getInstance(digestType));
     BitmapEncoder.saveBitmap(chart, output, BitmapEncoder.BitmapFormat.PNG);
+    BitmapEncoder.saveBitmap(chart, "/tmp/simplestExample.png", BitmapEncoder.BitmapFormat.PNG);
     output.close();
 
     // test
     assertImagesEquals("simplestExample.png", output);
   }
 
-  private Font arial() {
-    return new Font("Arial", Font.PLAIN, 14);
+  private Font fontToBeUsed() {
+    return new Font("Barlow", Font.PLAIN, 12);
   }
 
   public void assertImagesEquals(String expectedFileName, DigestOutputStream actual)
       throws Exception {
-    String path =
-        "/expectedChartRenderings/"
-            + System.getProperty("os.name").replaceAll(" ", "")
-            + "/"
-            + expectedFileName;
+    String path = "/expectedChartRenderings/" + expectedFileName;
     byte[] expectedBytes = Files.readAllBytes(Paths.get(getClass().getResource(path).toURI()));
     byte[] expectedDigest = MessageDigest.getInstance(digestType).digest(expectedBytes);
 
