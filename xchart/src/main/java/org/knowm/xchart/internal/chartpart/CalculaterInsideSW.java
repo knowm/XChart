b54a58a0652fdf59aa0ff9a259fddba424d809fb
JeonEunHui <64572911+JeonEunHui@users.noreply.github.com>
2022-05-25 10:27:42 +0900

    error correction

--- xchart/src/main/java/org/knowm/xchart/internal/chartpart/CalculaterInsideSW.java
+++ xchart/src/main/java/org/knowm/xchart/internal/chartpart/CalculaterInsideSW.java
@@ -1,12 +1,21 @@
 package org.knowm.xchart.internal.chartpart;
 
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.knowm.xchart.internal.chartpart.RenderableSeries.LegendRenderType;
+import org.knowm.xchart.internal.series.Series;
+import org.knowm.xchart.style.Styler;
 
 public class CalculaterInsideSW implements OffsetCalculater{
-	public double getXOffset(Chart<ST, S> chart, Rectangle2D bounds, int LEGEND_MARGIN) {
+	public double getXOffset(Chart chart, Rectangle2D bounds, int LEGEND_MARGIN) {
         return chart.getPlot().getBounds().getX() + LEGEND_MARGIN;
 	}
-	public double getYOffset(Chart<ST, S> chart, Rectangle2D bounds, int LEGEND_MARGIN) {
+	public double getYOffset(Chart chart, Rectangle2D bounds, int LEGEND_MARGIN) {
         return chart.getPlot().getBounds().getY()
                 + chart.getPlot().getBounds().getHeight()
                 - bounds.getHeight()
