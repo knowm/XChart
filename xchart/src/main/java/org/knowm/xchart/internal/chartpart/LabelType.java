a0ca3c43ef7e613a545056854645a00a410d6aa4
poobool-koon <mygm1302@gmail.com>
2022-05-27 12:35:34 +0900

    In progress

--- xchart/src/main/java/org/knowm/xchart/internal/chartpart/LabelType.java
+++ xchart/src/main/java/org/knowm/xchart/internal/chartpart/LabelType.java
@@ -1,5 +1,5 @@
 package org.knowm.xchart.internal.chartpart;
 
 public interface LabelType {
-    public abstract String getLabel(String name, Number y);
+    String getLabel(String name, Number y);
 }
