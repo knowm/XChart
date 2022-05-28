3fe4e926a71c61b9a542f90f2ad92994ce8aebf2
indl1670 <indl1670@gmail.com>
2022-05-28 17:15:38 +0900

    Apply factory method

---
+++ xchart/src/main/java/org/knowm/xchart/internal/chartpart/AxisTickCalculatorType.java
@@ -0,0 +1,9 @@
+package org.knowm.xchart.internal.chartpart;
+
+public enum AxisTickCalculatorType {
+  AxisTickCalculator_Callback,
+  AxisTickCalculator_Category,
+  AxisTickCalculator_Date,
+  AxisTickCalculator_Logarithmic,
+  AxisTickCalculator_Number
+}
