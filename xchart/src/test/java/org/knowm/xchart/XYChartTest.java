c71275f8d96892db02a07be6e6d185ab386e3492
DOHYEON98 <84163408+DOHYEON98@users.noreply.github.com>
2022-06-08 18:35:25 +0900

    /** 	* Purpose: test addSeries addSeries(String seriesName, float[] xData, float[] yData) 	* Input: addSeries(String seriesName, float[] xData, float[] yData) put seriesName = "test", xData = [3.0f, 4.0f, 5.0f], yData = [1.0f, 2.0f, 3.0f] 	* * Expected: seriesName = "test", xData = [3.0, 4.0, 5.0], yData = [1.0, 2.0, 3.0] 	  * 	*/

--- xchart/src/test/java/org/knowm/xchart/XYChartTest.java
+++ xchart/src/test/java/org/knowm/xchart/XYChartTest.java
@@ -205,5 +205,44 @@ public class XYChartTest {
 		assertThat(chart.getSeriesMap().get("test").getyAxisDataType()).isEqualTo(DataType.Number);
 		assertThat(chart.getSeriesMap().get("test").getExtraValues()).isEqualTo(null);
 	}
+	
+	/**
+	* Purpose: test addSeries addSeries(String seriesName, float[] xData, float[] yData)
+	* Input: addSeries(String seriesName, float[] xData, float[] yData) put seriesName = "test", xData = [3.0f, 4.0f, 5.0f], yData = [1.0f, 2.0f, 3.0f]
+	* * Expected: seriesName = "test", xData = [3.0, 4.0, 5.0], yData = [1.0, 2.0, 3.0]
+	  * 
+	*/
+	@Test
+	public void testAddSeriesFloatExtension2(){
+		//List<Integer> xData = new ArrayList<>();
+		float[] xData = {3.0f, 4.0f, 5.0f};
+		double[] xDataDouble = {3.0, 4.0, 5.0};
+		float[] yData = {1.0f, 2.0f, 3.0f};
+		double[] yDataDouble = {1.0, 2.0, 3.0};
+		
+		// Create Chart
+		XYChart chart =
+		    new XYChartBuilder()
+		        .width(800)
+		        .height(600)
+		        .title(getClass().getSimpleName())
+		        .xAxisTitle("Power")
+		        .yAxisTitle("Value")
+		        .build();
+		
+		// Customize Chart
+		chart.getStyler().setChartTitleVisible(true);
+		chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
+		chart.getStyler().setYAxisLogarithmic(true);
+		chart.getStyler().setXAxisLabelRotation(45);
+		
+		// Series
+		chart.addSeries("test",xData, yData);
+		assertThat(chart.getSeriesMap().get("test").getYData()).isEqualTo(yDataDouble);//if yData == float, convert from float to double
+		assertThat(chart.getSeriesMap().get("test").getXData()).isEqualTo(xDataDouble);// if xData == float, convert from float to double
+		assertThat(chart.getSeriesMap().get("test").getxAxisDataType()).isEqualTo(DataType.Number);
+		assertThat(chart.getSeriesMap().get("test").getyAxisDataType()).isEqualTo(DataType.Number);
+		assertThat(chart.getSeriesMap().get("test").getExtraValues()).isEqualTo(null);
+	}
 
 }
