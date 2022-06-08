e360db1eb16089d8933d8013f946c34d5648fc92
DOHYEON98 <84163408+DOHYEON98@users.noreply.github.com>
2022-06-08 18:25:13 +0900

    /** 	* Purpose: test constructor XYChart(int width, int height) 	* Input: XYChart(int width, int height) put Width = 3, Height = 5 	* * Expected: Width = 3, Height = 5, Styler = XYStyler 	  * 	*/

---
+++ xchart/src/test/java/org/knowm/xchart/XYChartTest.java
@@ -0,0 +1,51 @@
+package org.knowm.xchart;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.EmptyStackException;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.knowm.xchart.internal.Utils;
+import org.knowm.xchart.internal.series.Series.DataType;
+import org.knowm.xchart.internal.style.SeriesColorMarkerLineStyle;
+import org.knowm.xchart.internal.style.SeriesColorMarkerLineStyleCycler;
+import org.knowm.xchart.style.RadarStyler;
+import org.knowm.xchart.style.XYStyler;
+import org.knowm.xchart.style.Styler.ChartTheme;
+import org.knowm.xchart.style.Styler.LegendPosition;
+import org.knowm.xchart.style.theme.GGPlot2Theme;
+import org.knowm.xchart.style.theme.Theme;
+
+public class XYChartTest {
+	
+	/**
+	* Purpose: test constructor XYChart(int width, int height)
+	* Input: XYChart(int width, int height) put Width = 3, Height = 5
+	* * Expected: Width = 3, Height = 5, Styler = XYStyler
+	  * 
+	*/
+	@Test
+	public void testConstructor1(){
+		XYChart xyChart = new XYChart(3, 5);
+		
+		assertThat(xyChart.getWidth()).isEqualTo(3);
+		assertThat(xyChart.getHeight()).isEqualTo(5);
+		assertEquals((new XYStyler().getClass()), xyChart.getStyler().getClass());
+	}
+
+}
